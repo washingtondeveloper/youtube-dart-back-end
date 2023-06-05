89f01d72107a443921078af6a6259c9dae6b0ada
Washington Silva <washingtonprogramador@gmail.com>
2023-06-04 22:05:49 -0300

    aula 15

---
+++ dart-back-end_15/bin/server.dart
@@ -0,0 +1,8 @@
+import 'package:dart_back_end_14/config/config.dart';
+import 'package:dart_back_end_14/server/server.dart';
+
+void main() {
+  Server.bootstrap(controllers)
+      .then((server) => print('Server listening on port ${server.port}'))
+      .catchError(print);
+}
