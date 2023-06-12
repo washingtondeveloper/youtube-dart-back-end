0c350d38c23ead4c94ce1df0735af9d9effac102
Washington Silva <washingtonprogramador@gmail.com>
2023-06-11 22:15:40 -0300

    aula 16

---
+++ dart-back-end_16/lib/app/data/data.dart
@@ -0,0 +1,10 @@
+library data;
+
+import 'package:dart_back_end_16/app/domain/domain.dart';
+
+part 'services/clientes/get_clientes_service.dart';
+part 'services/clientes/add_cliente_service.dart';
+
+part 'gateway/clientes/get_clientes_gateway.dart';
+part 'gateway/clientes/add_cliente_gateway.dart';
+part 'gateway/clientes/get_cliente_by_email_gateway.dart';
---
+++ dart-back-end_16/lib/config/config.dart
@@ -0,0 +1,5 @@
+library config;
+
+import 'package:dart_back_end_16/app/api/api.dart';
+
+part 'main/main.dart';
