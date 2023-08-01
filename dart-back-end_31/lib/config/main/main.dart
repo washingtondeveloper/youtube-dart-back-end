1caa85f5084dcaffc6b42a81af4285188f0afca1
Washington Developer <washingtonprogramador@gmail.com>
2023-07-31 21:32:43 -0300

    aula 31

--- dart-back-end_31/lib/config/main/main.dart
+++ dart-back-end_31/lib/config/main/main.dart
@@ -2,16 +2,35 @@ part of config;
 
 final connection = PostgreSQL();
 
+final addClienteUseCase = AddClienteService(
+  addClienteGateway: AddClienteDAO(connection),
+  getClienteByEmailGateway: GetClienteByEmailDAO(connection),
+);
+
+final getClienteByIdUseCase = GetClienteByIdService(
+  getClienteByIdGateway: GetClienteByIdDAO(connection),
+);
+
+final deleteClienteUseCase = DeleteClienteService(
+  deleteClienteGateway: DeleteClienteDAO(connection),
+  getClienteByIdUseCase: getClienteByIdUseCase,
+);
+
+final getClientesUseCase = GetClientesService(
+  getClientesGateway: GetClientesDAO(connection: connection),
+);
+
+final updateClienteUseCase = UpdateClienteService(
+  updateClienteGateway: UpdateClienteDAO(connection),
+  getClienteByIdUseCase: getClienteByIdUseCase,
+);
+
 final controllers = <Controller>[
-  // ClientesController(
-  //   getClientesUseCase: GetClientesService(
-  //     getClientesGateway: GetClientesDAO(
-  //       connection: connection,
-  //     ),
-  //   ),
-  //   addClienteUseCase: AddClienteService(
-  //     addClienteGateway: AddClienteDAO(connection),
-  //     getClienteByEmailGateway: GetClienteByEmailDAO(connection),
-  //   ),
-  // ),
+  ClientesController(
+    addClienteUseCase: addClienteUseCase,
+    deleteClienteUseCase: deleteClienteUseCase,
+    getClienteByIdUseCase: getClienteByIdUseCase,
+    getClientesUseCase: getClientesUseCase,
+    updateClienteUseCase: updateClienteUseCase,
+  ),
 ];
